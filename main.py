0711d4a3c3d1b64e3904e8febd5e95d1a9027568
ayushzzzz <ayush.ghawana@gmail.com>
2025-08-16 18:33:38 +0530

    Data Transformer

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
 from network_security.components.data_ingestion import DataIngestion
 from network_security.components.data_validation import DataValidation
+from network_security.components.data_transformation import DataTransformation
 from network_security.exception.exception import NetworkSecurityException
 from network_security.logging.logger import logging
-from network_security.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from network_security.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from network_security.entity.config_entity import TrainingPipelineConfig
 
 if __name__=="__main__":
@@ -15,11 +16,17 @@ if __name__=="__main__":
         dataingestionartifact=data_ingestion.iniate_data_ingestion()
         print(dataingestionartifact)
         logging.info("Data Initiation Completed")
+
         data_validation_config = DataValidationConfig(trainingpipelineconfig)
         data_validation=DataValidation(dataingestionartifact,data_validation_config)
         data_validation_artifact = data_validation.initiate_data_validation()
         print(data_validation_artifact)
-
+        
+        data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
+        data_transformation = DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        
+        print(data_transformation_artifact)
         
 
     except Exception as e:
